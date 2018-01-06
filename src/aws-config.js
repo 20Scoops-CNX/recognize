411c2163d53393ffc225b8b34dd0fffac545cd4c
Elec <elecwebmaker@gmail.com>
2018-01-06 21:49:37 +0700

    add config file for aws

--- src/aws-config.js
+++ src/aws-config.js
@@ -1,1 +1,7 @@
 // test;
+const creds = new window.AWS.Credentials('AKIAJ7HW6B5R5Y3GBRTQ', 'n8Qs8QT2mpiAVhHkQaHOnnxlrw8GU0hKsraZgiOO');
+const myConfig = new window.AWS.Config({
+  credentials: creds, region: 'us-east-1'
+});
+
+window.AWS.config = myConfig;
