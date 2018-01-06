d2d6378ae88c4cbecdd93c7a3bcc3f98432851cd
Elec <elecwebmaker@gmail.com>
2018-01-06 18:33:24 +0700

    create WebCam component for tracking face in video camera

---
+++ src/WebCam/WebCam.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+
+class WebCam extends Component{
+  
+
+  componentDidMount(){
+    navigator.getUserMedia({video: true, audio: false}, (localMediaStream) => {
+      const tracker = new window.tracking.ObjectTracker('face');
+      this.videoRef.src = window.URL.createObjectURL(localMediaStream);
+      const ctx = this.canvasRef.getContext('2d');
+      let i = 0;
+      tracker.setInitialScale(4);
+      tracker.setStepSize(2);
+      tracker.setEdgesDensity(0.1);
+      this.videoRef.addEventListener('play', () => {
+        if (this.videoRef.paused || this.videoRef.ended) {
+          return;
+        }
+        const drawVideo = () => {
+          ctx.drawImage(this.videoRef, 0, 0, 500, 400);
+          i += 1;
+          if(i % 40 === 0){
+            tracker.track(ctx.getImageData(0,0, 500, 400).data, 500, 400);
+          }
+          window.requestAnimationFrame(drawVideo);
+        };
+        window.requestAnimationFrame(drawVideo);
+        
+      });
+      this.videoRef.play();
+      tracker.on('track', (event) => {
+        if (event.data.length !== 0) {
+          const base64Image = this.canvasRef.toDataURL("image/jpeg");
+          console.log('base64Image =>', base64Image);
+        }
+      });
+    }, () => {});
+  }
+
+  constructor(props){
+    super(props);
+  }
+  
+  render(){
+    console.log('test');
+    return (
+      <section>
+        <video id="video" width={500} height={400} autoPlay ref={(video) => { this.videoRef = video }} ></video>
+        <canvas width={500} height={400} ref={(canvas) => { this.canvasRef = canvas }} ></canvas>
+      </section>
+    );
+  }
+};
+
+export default WebCam;
