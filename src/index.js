a620d8ad1919959d155f01f3f6297cf16e21bba9
Elec <elecwebmaker@gmail.com>
2018-01-06 21:50:07 +0700

    import aws-sdk and aws config

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'tracking';
 import 'tracking/build/data/face';
+import 'aws-sdk';
+import './aws-config';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
