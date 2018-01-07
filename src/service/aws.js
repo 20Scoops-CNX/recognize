74198b250f738fd852bad0a9619ade996fe2d686
Elec <elecwebmaker@gmail.com>
2018-01-08 01:26:42 +0700

    fix eslint

--- src/service/aws.js
+++ src/service/aws.js
@@ -1,15 +1,15 @@
 // @flow
-import { dataURItoBlob } from './dataURItoBlob';
+import dataURItoBlob from './dataURItoBlob';
 
 const rekognition = new window.AWS.Rekognition();
 const collectionId: string = '123456789';
-export const rekoginition = (base64Image: string) => {
+export default (base64Image: string) => {
   rekognition.searchFacesByImage({
     CollectionId: collectionId,
-    Image:{
-      Bytes: dataURItoBlob(base64Image)
-    }
-  }, function (err, data) {
+    Image: {
+      Bytes: dataURItoBlob(base64Image),
+    },
+  }, (err, data) => {
     if (err) console.log(err, err.stack);
     else console.log(data);
   });
