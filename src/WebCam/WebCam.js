f2bc4ff9d2ffa1a59a4dcb33a3d02bdc3d99191e
Elec <elecwebmaker@gmail.com>
2018-01-20 22:21:06 +0700

    fix format

--- src/WebCam/WebCam.js
+++ src/WebCam/WebCam.js
@@ -3,38 +3,47 @@
 import * as React from 'react';
 import rekoginition from './../service/aws';
 
-
 class WebCam extends React.Component<{}> {
   componentDidMount() {
-    window.navigator.getUserMedia({ video: true, audio: false }, (localMediaStream) => {
-      const tracker: any = new window.tracking.ObjectTracker('face');
-      const { videoRef, canvasRef } = this;
-      if (canvasRef && videoRef) {
-        videoRef.src = window.URL.createObjectURL(localMediaStream);
-        const ctx = canvasRef.getContext('2d');
-        tracker.setInitialScale(4);
-        tracker.setStepSize(2);
-        tracker.setEdgesDensity(0.1);
-        videoRef.addEventListener('play', () => {
-          if (videoRef.paused || videoRef.ended) {
-            return;
-          }
-          window.requestAnimationFrame(this.drawVideo(ctx, tracker, videoRef));
-        });
-        videoRef.play();
-      }
-
-      tracker.on('track', (event) => {
-        if (event.data.length !== 0 && canvasRef) {
-          const base64Image = canvasRef.toDataURL('image/jpeg');
-          rekoginition(base64Image);
+    window.navigator.getUserMedia(
+      { video: true, audio: false },
+      localMediaStream => {
+        const tracker: any = new window.tracking.ObjectTracker('face');
+        const { videoRef, canvasRef } = this;
+        if (canvasRef && videoRef) {
+          videoRef.src = window.URL.createObjectURL(localMediaStream);
+          const ctx = canvasRef.getContext('2d');
+          tracker.setInitialScale(4);
+          tracker.setStepSize(2);
+          tracker.setEdgesDensity(0.1);
+          videoRef.addEventListener('play', () => {
+            if (videoRef.paused || videoRef.ended) {
+              return;
+            }
+            window.requestAnimationFrame(
+              this.drawVideo(ctx, tracker, videoRef)
+            );
+          });
+          videoRef.play();
         }
-      });
-    }, () => {});
+
+        tracker.on('track', event => {
+          if (event.data.length !== 0 && canvasRef) {
+            const base64Image = canvasRef.toDataURL('image/jpeg');
+            rekoginition(base64Image);
+          }
+        });
+      },
+      () => {}
+    );
   }
   canvasRef: ?HTMLCanvasElement;
   count: number = 0;
-  drawVideo(ctx: CanvasRenderingContext2D, tracker: any, videoRef: HTMLVideoElement) {
+  drawVideo(
+    ctx: CanvasRenderingContext2D,
+    tracker: any,
+    videoRef: HTMLVideoElement
+  ) {
     return () => {
       ctx.drawImage(videoRef, 0, 0, 500, 400);
       this.count += 1;
@@ -48,8 +57,23 @@ class WebCam extends React.Component<{}> {
   render() {
     return (
       <section>
-        <video id="video" width={500} height={400} autoPlay ref={(video) => { this.videoRef = video; }} />
-        <canvas style={{ display: 'none' }} width={500} height={400} ref={(canvas) => { this.canvasRef = canvas; }} />
+        <video
+          id="video"
+          width={500}
+          height={400}
+          autoPlay
+          ref={video => {
+            this.videoRef = video;
+          }}
+        />
+        <canvas
+          style={{ display: 'none' }}
+          width={500}
+          height={400}
+          ref={canvas => {
+            this.canvasRef = canvas;
+          }}
+        />
       </section>
     );
   }
--- src/aws-config.js
+++ src/aws-config.js
@@ -1,7 +1,11 @@
 // test;
-const creds = new window.AWS.Credentials('AKIAJ7HW6B5R5Y3GBRTQ', 'n8Qs8QT2mpiAVhHkQaHOnnxlrw8GU0hKsraZgiOO');
+const creds = new window.AWS.Credentials(
+  'AKIAJ7HW6B5R5Y3GBRTQ',
+  'n8Qs8QT2mpiAVhHkQaHOnnxlrw8GU0hKsraZgiOO'
+);
 const myConfig = new window.AWS.Config({
-  credentials: creds, region: 'us-east-1',
+  credentials: creds,
+  region: 'us-east-1'
 });
 
 window.AWS.config = myConfig;
