24b392c614d5af66cbac63a539b9ccaac5115a43
Elec <elecwebmaker@gmail.com>
2018-01-27 04:16:43 +0700

    update

--- src/aws-config.js
+++ src/aws-config.js
@@ -1,20 +1,17 @@
 // test;
 
 console.log(process.env.NODE_ENV, 'process.env.NODE_ENV');
-console.log(process.env.AWS_SECRET_KEY, 'process.env.AWS_SECRET_KEY');
 console.log(
-  process.env.AWS_SECRET_ACCESS_KEY,
+  process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
+  'process.env.AWS_SECRET_KEY'
+);
+console.log(
+  process.env.REACT_APP_AWS_SECRET_KEY,
   'process.env.AWS_SECRET_ACCESS_KEY'
 );
 
-const awsKey =
-  process.env.NODE_ENV === 'development'
-    ? process.env.REACT_APP_AWS_SECRET_ACCESS_KEY
-    : process.env.AWS_SECRET_KEY;
-const awsSecretKey =
-  process.env.NODE_ENV === 'development'
-    ? process.env.REACT_APP_AWS_SECRET_KEY
-    : process.env.AWS_SECRET_ACCESS_KEY;
+const awsKey = REACT_APP_AWS_SECRET_ACCESS_KEY;
+const awsSecretKey = process.env.REACT_APP_AWS_SECRET_KEY;
 const creds = new window.AWS.Credentials(awsKey, awsSecretKey);
 const myConfig = new window.AWS.Config({
   credentials: creds,
