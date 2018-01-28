211b8ff4d1238016c124bd3da75962347ac61c5a
Elec <elecwebmaker@gmail.com>
2018-01-28 20:58:25 +0700

    fix style

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 //  @flow
 
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 import WebCam from './components/WebCam/WebCam';
+import Header from './components/Header/Header';
 import ShowImageModal from './components/ShowImageModal/ShowImageModal';
 
 const App = () => (
   <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
-    </header>
+    <Header />
     <div>
       <WebCam />
       <ShowImageModal />
