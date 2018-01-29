c454326063fcfeda1ba2ba25724202ce8ee443be
Elecweb <elecwebmaker@gmail.com>
2018-01-29 10:45:47 +0700

    request rekoginition only when modal is close

--- src/actions/foundImage.js
+++ src/actions/foundImage.js
@@ -1,6 +1,7 @@
 // @flow
 
 import type { ACTION } from './helpers';
+import rekoginition from '../service/aws';
 
 export type foundImageAction = ACTION<{ image: string }>;
 
@@ -10,12 +11,23 @@ export const FOUND_IMAGE = 'FOUND_IMAGE';
 
 export const RESET_FOUND_IMAGE = 'RESET_FOUND_IMAGE';
 
-export const foundImage = (image: string): foundImageAction => ({
-  type: FOUND_IMAGE,
-  payload: {
-    image
+export const foundImage = (base64Image: string): foundImageAction => (
+  dispatch,
+  getState
+) => {
+  const { foundImage: foundImageState } = getState();
+  if (!foundImageState.isShow) {
+    return rekoginition(base64Image).then(image => {
+      dispatch({
+        type: FOUND_IMAGE,
+        payload: {
+          image
+        }
+      });
+    });
   }
-});
+  return null;
+};
 
 export const resetFoundImage = (): resetFoundImageAction => ({
   type: RESET_FOUND_IMAGE
